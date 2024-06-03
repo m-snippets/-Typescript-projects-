da1babb8826db5cf68e7a22a2d6b2f36daa97696
m-snippets <165569391+m-snippets@users.noreply.github.com>
2024-06-03 14:03:20 +0500

    Add files via upload

--- cli_number_game/number_game.ts
+++ cli_number_game/number_game.ts
@@ -1,1 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
 
+const rand_no = Math.floor(Math.random());
+//the answers object is in array
+const answers = await inquirer.prompt([
+	{
+		name: "userguessednumber",
+		type: "number",
+		message: "please a guess a number",
+	},
+]);
+
+if (answers.userguessednumber === rand_no) {
+	console.log("You guessed it right");
+} else {
+	console.log("You guessed it wrong");
+}
+//if error is provide a  name //then check for spelling mistakes
